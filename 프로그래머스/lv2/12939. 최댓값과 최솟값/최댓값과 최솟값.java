bb612ca8fc725b85de4b0bef97bbfa0906cd6305
casava840 <107945688+casava840@users.noreply.github.com>
2022-12-09 23:57:30 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 7.77 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.Comparator;
+class Solution {
+    public String solution(String s) {
+        StringTokenizer st = new StringTokenizer(s);
+        List<Integer> list = new ArrayList<>();
+        while (st.hasMoreTokens()){
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        list.sort(Comparator.naturalOrder());
+        int minNum = list.get(0);
+        int maxNum = list.get(list.size()-1);
+        String answer = minNum + " " + maxNum;
+        return answer;
+    }
+}
