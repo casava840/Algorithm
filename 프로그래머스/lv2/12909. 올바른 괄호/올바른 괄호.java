902399c0cea81ab089ad50a8e5b4a32455b128de
casava840 <107945688+casava840@users.noreply.github.com>
2022-12-09 23:57:06 +0900

    [level 2] Title: 올바른 괄호, Time: 6.01 ms, Memory: 52.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+class Solution {
+    boolean solution(String s) {
+        int count= 0;
+        boolean answer = true;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                count++;
+            }
+            else{
+                count--;
+                if(count<0){
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        if(count!=0){
+            answer = false;
+        }
+        return answer;
+    }
+}
