047d71fed1ec000510772317087718976efe930b
casava840 <107945688+casava840@users.noreply.github.com>
2023-02-23 22:17:10 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+def solution(want, number, discount):
+    answer = 0
+    for i in range(len(discount)-10+1):
+        c = Counter(discount[i:i+10])
+        flag = True
+        for j in range(len(want)):
+            if c[want[j]] < number[j]:
+                flag = False
+                break
+        if flag:
+            answer += 1
+    return answer
