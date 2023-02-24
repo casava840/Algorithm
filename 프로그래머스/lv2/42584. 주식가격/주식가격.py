6bad01d427530859658df1d03ad7e6c005eaab67
casava840 <107945688+casava840@users.noreply.github.com>
2023-02-24 22:17:46 +0900

    [level 2] Title: 주식가격, Time: 44.01 ms, Memory: 17.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(prices):
+    answer = []
+    deq = deque(prices)
+    while len(deq) > 2:
+        price = deq.popleft()
+        flag = True
+        for i, d in enumerate(deq):
+            if price > d:
+                flag = False
+                answer.append(i+1)
+                break
+        if flag:
+            answer.append(len(deq))
+    answer.append(1)
+    answer.append(0)
+            
+    return answer 
