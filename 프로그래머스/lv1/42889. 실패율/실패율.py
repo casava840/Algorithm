53fa4f80554405be63b8379e92798aefda3190d9
casava840 <107945688+casava840@users.noreply.github.com>
2023-08-21 23:21:40 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,11 +1,41 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
 def solution(N, stages):
-    result = {}
-    denominator = len(stages)
-    for stage in range(1, N+1):
-        if denominator != 0:
-            count = stages.count(stage)
-            result[stage] = count / denominator
-            denominator -= count
+    answer = []
+    rate = []
+    fail = [0]*N
+    total_user = len(stages)
+    while stages:
+        temp = stages.pop(-1)
+        if temp < N+1:
+            fail[temp-1] += 1
+    for i in range(N):
+        if fail[i] != 0:
+            rate.append([fail[i]/total_user,i+1])
+            total_user -= fail[i]
         else:
-            result[stage] = 0
-    return sorted(result, key=lambda x : result[x], reverse=True)
+            rate.append([0,i+1])
+
+    rate.sort(key = lambda x: x[0] ,reverse=True)
+    for i in range(len(rate)):
+        answer.append(rate[i][1])
+    return answer
