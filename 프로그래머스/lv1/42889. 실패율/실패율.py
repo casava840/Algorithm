13703cadbcaaf2be483bd78530f0bda39bda8a5d
casava840 <107945688+casava840@users.noreply.github.com>
2023-08-21 23:14:30 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -0,0 +1,20 @@
+def solution(N, stages):
+    answer = []
+    rate = []
+    fail = [0]*N
+    total_user = len(stages)
+    while stages:
+        temp = stages.pop(-1)
+        if temp < N+1:
+            fail[temp-1] += 1
+    for i in range(N):
+        if fail[i] != 0:
+            rate.append([fail[i]/total_user,i+1])
+            total_user -= fail[i]
+        else:
+            rate.append([0,i+1])
+    
+    rate.sort(key = lambda x: x[0] ,reverse=True)
+    for i in range(len(rate)):
+        answer.append(rate[i][1])
+    return answer
