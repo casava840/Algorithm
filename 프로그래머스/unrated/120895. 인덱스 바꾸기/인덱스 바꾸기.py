eabb20919affee1385ce36690b58d623197a14f2
casava840 <107945688+casava840@users.noreply.github.com>
2023-10-14 23:04:21 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -0,0 +1,4 @@
+def solution(my_string, num1, num2):
+    if num1 > num2:
+        num1, num2 = num2, num1
+    return my_string[0:num1] + my_string[num2] + my_string[num1+1:num2] + my_string[num1] + my_string[num2+1:]
